2d4c93010c81bcdac4c161708f9241196323bede
yuvraj <yuvi.unique@gmail.com>
2022-08-30 09:03:34 +0530

    Added pointer concept

---
+++ pointer/pointeraritemetictest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    int a = 7;
+    int *c = &a;
+    c = c + 1;
+    cout << a << "  " << *c << endl; */
+
+    /* int a = 7;
+    int *c = &a;
+    c = c + 3;
+    cout << c << endl; */
+
+    int a[] = {1, 2, 3, 4};
+    int *p = a+1;
+    cout << *p << endl;
+}
---
+++ pointer/test_char_pointers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    char b[] = "xyz";
+    char *c = &b[0];
+    cout << c << endl; */
+
+    /*
+    char b[] = "xyz";
+    char *c = &b[0];
+    c++;
+    cout << c << endl; */
+
+    char s[] = "hello";
+    char *p = s;
+    cout << s[0] << " " << p[0];
+}
