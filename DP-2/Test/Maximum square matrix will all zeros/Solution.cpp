772325d61a50ec4db160a088417cbed13b52083a
yuvraj <yuvi.unique@gmail.com>
2022-11-22 21:28:53 +0530

    Added new assignments

--- DP-2/Test/Maximum square matrix will all zeros/Solution.cpp
+++ DP-2/Test/Maximum square matrix will all zeros/Solution.cpp
@@ -4,29 +4,25 @@ using namespace std;
 
 int findMaxSquareWithAllZeros(int **arr, int row, int col)
 {
-    // Write your code here
-    int dp[row][col] = {0};
+	//Write your code here
+
+	 int dp[row+1][col+1] = {0};
 
     // Initialization
     int ans = 0;
     for (int i = 0; i < row; i++)
     {
-        dp[i][0] = (arr[i][0]) ? 0 : 1;
-        ans = max(ans, dp[i][0]);
+        dp[i][0] = (arr[i][0]) ? 0 : 1;       
     }
     for (int i = 0; i < col; i++)
     {
-        dp[0][i] = (arr[0][i]) ? 0 : 1;
-        ans = max(ans, dp[0][i]);
+        dp[0][i] = (arr[0][i]) ? 0 : 1;      
     }
 
-    // Filling up dp table. Where each entry at (i,j) tells of largest size of the subsquare possible
-    // ending at (i,j);
     for (int i = 1; i < row; i++)
     {
         for (int j = 1; j < col; j++)
         {
-
             if (arr[i][j] == 1)
                 dp[i][j] = 0;
             else
